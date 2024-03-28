326832ae06d8cfac9367e7f2895dab5afd516c95
Rick Cheng <rick@agora.io>
2024-03-28 11:27:38 -0700

    Working simulator

---
+++ Assets/AgoraVP/AgoraVPManager.cs
@@ -0,0 +1,248 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+using System.Collections.Generic;
+using Agora.Rtc;
+using Agora.Rtc.Utils;
+
+namespace Agora_RTC_Plugin.API_Example
+{
+    public class AgoraVPManager : MonoBehaviour
+    {
+        [Header("_____________Basic Configuration_____________")]
+        [FormerlySerializedAs("APP_ID")]
+        [SerializeField]
+        private string _appID = "";
+
+        [FormerlySerializedAs("TOKEN")]
+        [SerializeField]
+        private string _token = "";
+
+        [FormerlySerializedAs("CHANNEL_NAME")]
+        [SerializeField]
+        private string _channelName = "";
+
+        [SerializeField]
+        internal GameObject ViewContainerPrefab;
+
+        [SerializeField]
+        GameObject TargetObject; // to be looked at
+
+        internal IRtcEngine RtcEngine = null;
+
+        internal Dictionary<uint, GameObject> ViewObjects = new Dictionary<uint, GameObject>();
+
+        private void Start()
+        {
+            if (CheckAppId())
+            {
+                InitEngine();
+                SetBasicConfiguration();
+                JoinChannel();
+            }
+        }
+
+
+        // Update is called once per frame
+        private void Update()
+        {
+            PermissionHelper.RequestMicrophontPermission();
+            PermissionHelper.RequestCameraPermission();
+            foreach(var ob in ViewObjects.Values) {
+                ob.transform.LookAt(TargetObject.transform);
+	        }
+        }
+
+        private void OnDestroy()
+        {
+            Debug.Log("OnDestroy");
+            if (RtcEngine == null) return;
+            RtcEngine.InitEventHandler(null);
+            RtcEngine.LeaveChannel();
+            RtcEngine.Dispose();
+        }
+
+        private bool CheckAppId()
+        {
+            Debug.Assert(_appID.Length > 10, "Please fill in your appId in API-Example/profile/appIdInput.asset");
+            Debug.Log("Running platform is " + Application.platform);
+            return _appID.Length > 10;
+        }
+
+        private void InitEngine()
+        {
+            RtcEngine = Agora.Rtc.RtcEngine.CreateAgoraRtcEngine();
+            UserEventHandler handler = new UserEventHandler(this);
+            RtcEngineContext context = new RtcEngineContext(_appID, 0,
+                                        CHANNEL_PROFILE_TYPE.CHANNEL_PROFILE_LIVE_BROADCASTING,
+                                        AUDIO_SCENARIO_TYPE.AUDIO_SCENARIO_GAME_STREAMING, AREA_CODE.AREA_CODE_GLOB);
+            RtcEngine.Initialize(context);
+            RtcEngine.InitEventHandler(handler);
+        }
+
+        private void SetBasicConfiguration()
+        {
+            RtcEngine.EnableAudio();
+            RtcEngine.EnableVideo();
+            VideoEncoderConfiguration config = new VideoEncoderConfiguration();
+            config.dimensions = new VideoDimensions(640, 360);
+            config.frameRate = 15;
+            config.bitrate = 0;
+            RtcEngine.SetVideoEncoderConfiguration(config);
+            RtcEngine.SetChannelProfile(CHANNEL_PROFILE_TYPE.CHANNEL_PROFILE_LIVE_BROADCASTING);
+            RtcEngine.SetClientRole(CLIENT_ROLE_TYPE.CLIENT_ROLE_BROADCASTER);
+
+            // For now this private API is needed to make voice chat working
+            //if (Application.platform == RuntimePlatform.VisionOS)
+            {
+                RtcEngine.SetParameters("che.audio.restartWhenInterrupted", true);
+            }
+        }
+
+        #region -- Button Events ---
+
+        public void JoinChannel()
+        {
+            RtcEngine.JoinChannel(_token, _channelName);
+        }
+
+        public void LeaveChannel()
+        {
+            RtcEngine.LeaveChannel();
+        }
+
+        public void StartPublish()
+        {
+            var options = new ChannelMediaOptions();
+            options.publishMicrophoneTrack.SetValue(true);
+            options.publishCameraTrack.SetValue(true);
+            var nRet = RtcEngine.UpdateChannelMediaOptions(options);
+            Debug.Log("UpdateChannelMediaOptions: " + nRet);
+        }
+
+        public void StopPublish()
+        {
+            var options = new ChannelMediaOptions();
+            options.publishMicrophoneTrack.SetValue(false);
+            options.publishCameraTrack.SetValue(false);
+            var nRet = RtcEngine.UpdateChannelMediaOptions(options);
+            Debug.Log("UpdateChannelMediaOptions: " + nRet);
+        }
+
+        public void AdjustVideoEncodedConfiguration640()
+        {
+            VideoEncoderConfiguration config = new VideoEncoderConfiguration();
+            config.dimensions = new VideoDimensions(640, 360);
+            config.frameRate = 15;
+            config.bitrate = 0;
+            RtcEngine.SetVideoEncoderConfiguration(config);
+        }
+
+        public void AdjustVideoEncodedConfiguration480()
+        {
+            VideoEncoderConfiguration config = new VideoEncoderConfiguration();
+            config.dimensions = new VideoDimensions(480, 480);
+            config.frameRate = 15;
+            config.bitrate = 0;
+            RtcEngine.SetVideoEncoderConfiguration(config);
+        }
+        #endregion
+
+        internal class UserEventHandler : IRtcEngineEventHandler
+        {
+            private readonly AgoraVPManager _app;
+
+            internal UserEventHandler(AgoraVPManager videoSample)
+            {
+                _app = videoSample;
+            }
+
+            public override void OnError(int err, string msg)
+            {
+                Debug.Log(string.Format("OnError err: {0}, msg: {1}", err, msg));
+            }
+
+            public override void OnJoinChannelSuccess(RtcConnection connection, int elapsed)
+            {
+                int build = 0;
+                Debug.Log("Agora: OnJoinChannelSuccess ");
+                Debug.Log(string.Format("sdk version: ${0}",
+                    _app.RtcEngine.GetVersion(ref build)));
+                Debug.Log(string.Format("sdk build: ${0}",
+                  build));
+                Debug.Log(
+                    string.Format("OnJoinChannelSuccess channelName: {0}, uid: {1}, elapsed: {2}",
+                                    connection.channelId, connection.localUid, elapsed));
+                Vector3 pos = new Vector3(-2.5f, 0, 3.28f);
+                CreateUserView(0, connection.channelId, pos);
+
+                /*
+                var view = AgoraViewUtils.MakeVideoView(0, connection.channelId, AgoraViewUtils.DisplayContainerType.CustomMesh, _app.ViewContainerPrefab);
+                GameObject nobj = new GameObject();
+                nobj.name = view.name + "_parent";
+                nobj.transform.SetParent(_app.transform);
+                nobj.transform.localScale = 0.2f * Vector3.one;
+                nobj.transform.rotation = Quaternion.identity;
+
+                view.transform.SetParent(nobj.transform);
+                view.transform.localPosition = new Vector3(0, 0, 6);
+                */
+            }
+
+            public override void OnRejoinChannelSuccess(RtcConnection connection, int elapsed)
+            {
+                Debug.Log("OnRejoinChannelSuccess");
+            }
+
+            public override void OnLeaveChannel(RtcConnection connection, RtcStats stats)
+            {
+                Debug.Log("OnLeaveChannel");
+            }
+
+            public override void OnClientRoleChanged(RtcConnection connection, CLIENT_ROLE_TYPE oldRole, CLIENT_ROLE_TYPE newRole, ClientRoleOptions newRoleOptions)
+            {
+                Debug.Log("OnClientRoleChanged");
+            }
+
+            public override void OnUserJoined(RtcConnection connection, uint uid, int elapsed)
+            {
+                Debug.Log(string.Format("OnUserJoined uid: ${0} elapsed: ${1}", uid, elapsed));
+                var count = _app.transform.childCount;
+                Vector3 pos = new Vector3(count * 2.5f, 0, 3.28f);
+                CreateUserView(uid, connection.channelId, pos);
+            }
+
+            private void CreateUserView(uint uid, string channelId, Vector3 parentLocation)
+            {
+                var view = AgoraViewUtils.MakeVideoView(uid, channelId, AgoraViewUtils.DisplayContainerType.CustomMesh, _app.ViewContainerPrefab);
+                GameObject nobj = new GameObject();
+                nobj.name = view.name + "_parent";
+                nobj.transform.SetParent(_app.transform);
+                nobj.transform.localScale = 0.2f * Vector3.one;
+                nobj.transform.localPosition = parentLocation;
+                nobj.transform.rotation = Quaternion.identity;
+
+                view.transform.Rotate(0f, 180.0f, 0.0f);
+                view.transform.SetParent(nobj.transform);
+                view.transform.localPosition = Vector3.zero;
+                _app.ViewObjects[uid] = nobj;
+            }
+            public override void OnUserOffline(RtcConnection connection, uint uid, USER_OFFLINE_REASON_TYPE reason)
+            {
+                Debug.Log(string.Format("OnUserOffLine uid: ${0}, reason: ${1}", uid,
+                    (int)reason));
+                if (_app.ViewObjects.ContainsKey(uid)) _app.ViewObjects.Remove(uid);
+                AgoraViewUtils.DestroyVideoView(uid);
+            }
+
+            public override void OnUplinkNetworkInfoUpdated(UplinkNetworkInfo info)
+            {
+             //   Debug.Log("OnUplinkNetworkInfoUpdated");
+            }
+
+            public override void OnDownlinkNetworkInfoUpdated(DownlinkNetworkInfo info)
+            {
+             //   Debug.Log("OnDownlinkNetworkInfoUpdated");
+            }
+        }
+    }
+}
---
+++ Assets/AgoraVP/AgoraViewUtils.cs
@@ -0,0 +1,152 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Agora_RTC_Plugin.API_Example;
+
+namespace Agora.Rtc.Utils
+{
+    public class AgoraViewUtils
+    {
+        internal enum DisplayContainerType
+        {
+            RawImage,
+            Plane,
+            CustomMesh
+        };
+
+        internal static GameObject MakeVideoView(uint uid, string channelId, DisplayContainerType displayType, GameObject prefab = null)
+        {
+            var go = GameObject.Find(uid.ToString());
+            if (!ReferenceEquals(go, null))
+            {
+                return go; // reuse
+            }
+
+            // create a GameObject and assign to this new user
+            VideoSurface videoSurface = null;
+
+            switch (displayType)
+            {
+                case DisplayContainerType.RawImage:
+                    videoSurface = MakeImageSurface(uid.ToString());
+                    break;
+                case DisplayContainerType.Plane:
+                    videoSurface = MakePlaneSurface(uid.ToString());
+                    break;
+                case DisplayContainerType.CustomMesh:
+                    if (prefab != null)
+                    {
+                        videoSurface = MakeCustomMesh(uid.ToString(), prefab);
+                    }
+                    else
+                    {
+                        Debug.LogError("Prefab is NULL!");
+                        return null;
+                    }
+                    break;
+            }
+
+            if (videoSurface == null) return null;
+
+            // configure videoSurface
+            if (uid == 0)
+            {
+                videoSurface.SetForUser(uid, channelId);
+            }
+            else
+            {
+                videoSurface.SetForUser(uid, channelId, VIDEO_SOURCE_TYPE.VIDEO_SOURCE_REMOTE);
+            }
+
+            videoSurface.OnTextureSizeModify += (int width, int height) =>
+            {
+                float scale = (float)height / (float)width;
+                videoSurface.transform.localScale = new Vector3(-5, 1, 5 * scale);
+                Debug.Log("OnTextureSizeModify: " + width + "  " + height);
+            };
+
+            videoSurface.SetEnable(true);
+            videoSurface.gameObject.name = "videosurface_" + uid;
+            return videoSurface.gameObject;
+        }
+
+        private static VideoSurface MakeCustomMesh(string goName, GameObject prefab)
+        {
+            var go = GameObject.Instantiate(prefab);
+            // configure videoSurface
+            var videoSurface = go.AddComponent<VideoSurface>();
+            go.transform.Rotate(-90.0f, 0.0f, 0.0f);
+            return videoSurface;
+        }
+        // put a video surface on Plane
+        private static VideoSurface MakePlaneSurface(string goName)
+        {
+            var go = GameObject.CreatePrimitive(PrimitiveType.Plane);
+
+            if (go == null)
+            {
+                return null;
+            }
+
+            go.name = goName;
+            var mesh = go.GetComponent<MeshRenderer>();
+            if (mesh != null)
+            {
+                Debug.LogWarning("VideoSureface update shader");
+                mesh.material = new Material(Shader.Find("Unlit/Texture"));
+            }
+            // set up transform
+            go.transform.Rotate(-90.0f, 0.0f, 0.0f);
+
+            // configure videoSurface
+            var videoSurface = go.AddComponent<VideoSurface>();
+            return videoSurface;
+        }
+
+        // put a video surface on RawImage
+        private static VideoSurface MakeImageSurface(string goName)
+        {
+            GameObject go = new GameObject();
+
+            if (go == null)
+            {
+                return null;
+            }
+
+            go.name = goName;
+            // to be renderered onto
+            go.AddComponent<RawImage>();
+            // make the object draggable
+            go.AddComponent<UIElementDrag>();
+            var canvas = GameObject.Find("VideoCanvas");
+            if (canvas != null)
+            {
+                go.transform.parent = canvas.transform;
+                Debug.Log("add video view");
+            }
+            else
+            {
+                Debug.Log("Canvas is null video view");
+            }
+
+            // set up transform
+            go.transform.Rotate(0f, 0.0f, 180.0f);
+            go.transform.localPosition = Vector3.zero;
+            go.transform.localScale = new Vector3(2f, 3f, 1f);
+
+            // configure videoSurface
+            var videoSurface = go.AddComponent<VideoSurface>();
+            return videoSurface;
+        }
+
+        internal static void DestroyVideoView(uint uid)
+        {
+            var go = GameObject.Find("videosurface_" + uid.ToString());
+            if (!ReferenceEquals(go, null))
+            {
+                GameObject.Destroy(go);
+            }
+        }
+
+
+    }
+}
